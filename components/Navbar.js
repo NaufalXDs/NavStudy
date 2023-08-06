848eee9e8c58f477f528da9455c4a238d58cb52e
Naufal <naufal@naufalxd.xyz>
2023-08-06 08:00:01 +0700

    fix hreft

--- components/Navbar.js
+++ components/Navbar.js
@@ -42,13 +42,13 @@ export default function Navbar() {
             </button>
 
             <div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 w-56 hidden z-10 mt-2 min-w-[15rem] bg-white shadow-md rounded-lg p-2 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700" aria-labelledby="hs-dropdown-basic">
-              <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500  dark:hover:bg-gray-700 dark:text-sky-500  dark:hover:text-gray-500" href="#">
+              <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500  dark:hover:bg-gray-700 dark:text-sky-500  dark:hover:text-gray-500" href="/">
                 Home
               </a>
-              <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500  dark:hover:bg-gray-700 dark:text-gray-400  dark:hover:text-gray-500" href="#">
+              <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500  dark:hover:bg-gray-700 dark:text-gray-400  dark:hover:text-gray-500" href="/article">
                 Article
               </a>
-              <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500  dark:hover:bg-gray-700 dark:text-gray-400  dark:hover:text-gray-500" href="#">
+              <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500  dark:hover:bg-gray-700 dark:text-gray-400  dark:hover:text-gray-500" href="/blog">
                 Blog
               </a>
             </div>
