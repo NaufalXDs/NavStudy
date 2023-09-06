22ef453696753e6123e22deeaaa1306a35aa41e5
Naufal <naufal@naufalxd.xyz>
2023-09-06 13:13:06 +0700

    Edit Text 404

--- pages/404.js
+++ pages/404.js
@@ -10,7 +10,7 @@ export default function Custom404() {
                 {/* <video src="/sorry.mp4" className="text-center items-center jus" height="auto" width={200} ></video> */}
                 <iframe className="mt-4 w-[80%] mx-auto" src="https://open.spotify.com/embed/playlist/2fdifReMjfs30VM9aDpxyi?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 <br />
-                <h1 className="text-center items-center text-lg mb-4 text-white">404 - Page Not Found</h1>
+                <h1 className="text-center items-center text-lg mb-4 text-white">404 - Page Not Found :)</h1>
             </Container>
         </Layout>
     )
