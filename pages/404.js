836ad398ed1ce7f146c1472646106e100680b084
Naufal <naufal@naufalxd.xyz>
2023-09-09 22:59:38 +0700

    1

--- pages/404.js
+++ pages/404.js
@@ -9,7 +9,7 @@ export default function Custom404() {
                 {/* <img src="/sorry.mp4" height="auto" width={20} /> */}
                 {/* <video src="/sorry.mp4" className="text-center items-center jus" height="auto" width={200} ></video> */}
                 <iframe className="mt-4 w-[80%] mx-auto" src="https://open.spotify.com/embed/playlist/2fdifReMjfs30VM9aDpxyi?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                <br />
+                         <br />
                 <h1 className="text-center items-center text-lg mb-4 text-white">404 - Page Not Found :)</h1>
             </Container>
         </Layout>
