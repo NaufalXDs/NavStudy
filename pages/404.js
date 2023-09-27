4821d17e5a577a6e245c936ce0dc7b6583b259cf
Naufal <naufal@naufalxd.xyz>
2023-09-27 21:41:22 +0700

    Fix Footer in mobile view

--- pages/404.js
+++ pages/404.js
@@ -8,9 +8,9 @@ export default function Custom404() {
             <Container className="container mx-auto px-10">
                 {/* <img src="/sorry.mp4" height="auto" width={20} /> */}
                 {/* <video src="/sorry.mp4" className="text-center items-center jus" height="auto" width={200} ></video> */}
-                <iframe className="mt-4 w-[80%] mx-auto" src="https://open.spotify.com/embed/playlist/2fdifReMjfs30VM9aDpxyi?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe className="mt-4 w-[80%] mx-auto" src="https://open.spotify.com/embed/playlist/2fdifReMjfs30VM9aDpxyi?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                          <br />
-                <h1 className="text-center items-center text-lg mb-4 text-white">404 - Page Not Found :)</h1>
+                <h1 className="text-center items-center text-lg mt-10 md:mb-48 mb-24 text-white s">404 - Page Not Found :)</h1>
             </Container>
         </Layout>
     )
